83bf9b46753b094a622b4d8cc3c0532faf2219ca
Manuel <manuelortiz12mj@gmail.com>
2024-05-22 09:30:35 +0200

    Documentación 1 Manuel Ortiz

--- Estudiante.py
+++ Estudiante.py
@@ -11,6 +11,9 @@ Extrae una superclase con los campos
 
 class Persona:
     def __init__(self):
+        """
+
+        """
         self.__nif = value
         self.__nombre = value
         self.__apellidos = value
@@ -23,6 +26,13 @@ class Estudiante(Persona):
     apellidos = "Apellidos";
 
     def __init__(self, nif, curso, nombre, apellidos):
+        """
+        Constructor de la clase Estudiante, que deriva de persona
+        :param nif: dni del alumno
+        :param curso: curso del alumno
+        :param nombre: nombre del alumno
+        :param apellidos: apellidos del alumno
+        """
         self.nif = nif;
         self.curso = curso;
         self.nombre = nombre;
@@ -30,33 +40,65 @@ class Estudiante(Persona):
 
     @property
     def nif(self):
+        """
+        devuelve nif
+        :return:
+        """
         return self.__nif
 
     @nif.setter
     def nif(self, value):
+        """
+        setter del nif
+        :param value:
+        """
         self.__nif = value
 
     @property
     def curso(self):
+        """
+        Devuelve el curso
+        :return:
+        """
         return self.__curso
 
     @curso.setter
     def curso(self, value):
+        """
+        Setter para curso
+        :param value:
+        """
         self.__curso = value
 
     @property
     def nombre(self):
+        """
+        devuelve nombre
+        :return:
+        """
         return self.__nombre
 
     @nombre.setter
     def nombre(self, value: int):
+        """
+        setter del nombre
+        :param value:
+        """
         self.__nombre = value
 
     @property
     def apellidos(self):
+        """
+        devuelve apellidos
+        :return:
+        """
         return self.__apellidos
 
     @apellidos.setter
     def apellidos(self, value):
+        """
+        setter de apellidos
+        :param value:
+        """
         self.__apellidos = value
 
